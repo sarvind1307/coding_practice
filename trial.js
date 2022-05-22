b0db0f5cfa7160f88d162498426e57b3490e8539
S Arvind <sarvind1307@gmail.com>
2022-05-22 20:54:13 +0530

    22/05/2022

---
+++ trial.js
@@ -0,0 +1,18 @@
+// console.log(21)
+/*
+console.log('JavaScript');
+console.log(2011);
+console.log('Woohoo! I love to code! #codecademy');
+console.log(20.49);
+*/
+
+/*
+console.log(21 + 3.5);
+console.log(2022 - 1969);
+console.log(65 / 240);
+console.log(0.2708 * 100)
+*/
+
+/*
+
+*/
